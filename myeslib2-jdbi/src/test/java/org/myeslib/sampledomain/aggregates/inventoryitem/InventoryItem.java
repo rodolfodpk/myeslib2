b2c4cb0a84b0c2748645a9300fbd26de11e65506
Rodolfo de Paula <rpaula.example@git.vmcommerce.intra>
2015-01-12 14:16:45 -0200

    fix @EqualsAndHashCode and @ToString annotation params

--- myeslib2-jdbi/src/test/java/org/myeslib/sampledomain/aggregates/inventoryitem/InventoryItem.java
+++ myeslib2-jdbi/src/test/java/org/myeslib/sampledomain/aggregates/inventoryitem/InventoryItem.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
-@Builder @EqualsAndHashCode(exclude = "service,bus") @ToString(exclude = "service,bus")
+@Builder @EqualsAndHashCode(exclude = {"service", "bus"})  @ToString(exclude = {"service", "bus"})
 public class InventoryItem implements AggregateRoot {
 
     private UUID id;
