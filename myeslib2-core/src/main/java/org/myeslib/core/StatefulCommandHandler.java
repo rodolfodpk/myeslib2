1e924e68589b71168d187e2c81b3c34af5fa11d9
Rodolfo de Paula <rpaula.example@git.vmcommerce.intra>
2015-02-04 22:15:53 -0200

    added StatefulCommandHandler interface just in cases where is necessary to subvert CQRS

---
+++ myeslib2-core/src/main/java/org/myeslib/core/StatefulCommandHandler.java
@@ -0,0 +1,17 @@
+package org.myeslib.core;
+
+import net.jcip.annotations.NotThreadSafe;
+import org.myeslib.data.UnitOfWork;
+
+import java.util.Optional;
+
+/**
+ * If the result of command handling _must_ be accessible (thus violating CQRS), the Command Handler can also implement
+ * this interface in order to expose the resulting UnitOfWork.
+ */
+@NotThreadSafe
+public interface StatefulCommandHandler {
+
+    Optional<UnitOfWork> getUnitOfWork();
+
+}
--- myeslib2-jdbi/src/test/java/org/myeslib/sampledomain/aggregates/inventoryitem/handlers/DecreaseHandler.java
+++ myeslib2-jdbi/src/test/java/org/myeslib/sampledomain/aggregates/inventoryitem/handlers/DecreaseHandler.java
@@ -1,5 +1,6 @@
 package org.myeslib.sampledomain.aggregates.inventoryitem.handlers;
 
+import org.myeslib.core.StatefulCommandHandler;
 import org.myeslib.data.Snapshot;
 import org.myeslib.data.UnitOfWork;
 import org.myeslib.core.CommandHandler;
@@ -11,12 +12,14 @@ import org.myeslib.infra.UnitOfWorkJournal;
 import org.myeslib.infra.InteractionContext;
 
 import javax.inject.Inject;
+import java.util.Optional;
 import java.util.UUID;
 
-public class DecreaseHandler implements CommandHandler<DecreaseInventory> {
+public class DecreaseHandler implements CommandHandler<DecreaseInventory>, StatefulCommandHandler {
 
     final UnitOfWorkJournal<UUID> journal;
     final SnapshotReader<UUID, InventoryItem> snapshotReader;
+    private Optional<UnitOfWork> unitOfWork = Optional.empty();
 
     @Inject
     public DecreaseHandler(UnitOfWorkJournal<UUID> journal, SnapshotReader<UUID, InventoryItem> snapshotReader) {
@@ -30,7 +33,12 @@ public class DecreaseHandler implements CommandHandler<DecreaseInventory> {
         final InteractionContext interactionContext = new MultiMethodInteractionContext(aggregateRoot);
         aggregateRoot.setInteractionContext(interactionContext);
         aggregateRoot.decrease(command.howMany());
-        final UnitOfWork unitOfWork = UnitOfWork.create(UUID.randomUUID(), command.commandId(), snapshot.getVersion(), interactionContext.getAppliedEvents());
-        journal.append(command.targetId(), command.commandId(), command, unitOfWork);
+        this.unitOfWork = Optional.of(UnitOfWork.create(UUID.randomUUID(), command.commandId(), snapshot.getVersion(), interactionContext.getAppliedEvents()));
+        journal.append(command.targetId(), command.commandId(), command, unitOfWork.get());
+    }
+
+    @Override
+    public Optional<UnitOfWork> getUnitOfWork() {
+        return unitOfWork;
     }
 }
