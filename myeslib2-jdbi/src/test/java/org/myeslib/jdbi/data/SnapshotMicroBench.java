2fea16d30911d9b1a79879754217f51e10cac59e
Rodolfo de Paula <rpaula.example@git.vmcommerce.intra>
2015-02-06 02:59:21 -0200

    micro bench for Snapshot<A> impls: Kryo and Gson based

---
+++ myeslib2-jdbi/src/test/java/org/myeslib/jdbi/data/SnapshotMicroBench.java
@@ -0,0 +1,61 @@
+package org.myeslib.jdbi.data;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.google.gson.Gson;
+import org.myeslib.data.Snapshot;
+import org.myeslib.sampledomain.aggregates.inventoryitem.InventoryItem;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SnapshotMicroBench {
+
+    static Kryo kryo = new Kryo();
+    static Gson gson = new Gson();
+
+    public static void main(String[] args) throws RunnerException {
+
+        kryo.register(InventoryItem.class);
+
+        Options opt = new OptionsBuilder()
+                .include(SnapshotMicroBench.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @Benchmark
+    public void kryo() {
+
+        final InventoryItem item = InventoryItem.builder().id(UUID.randomUUID()).description("item4test").available(0).build();
+        final InventoryItem identicalItem = InventoryItem.builder().id(item.getId()).description(item.getDescription()).available(item.getAvailable()).build();
+        final Snapshot<InventoryItem> snapshot = new JdbiKryoSnapshot<>(item, 0L, kryo);
+        assertThat(snapshot.getAggregateInstance(), is(item));
+        final InventoryItem itemFromSnapshot = snapshot.getAggregateInstance();
+        itemFromSnapshot.setDescription("notAnymore");
+        assertThat(snapshot.getAggregateInstance(), is(identicalItem));
+
+    }
+
+    @Benchmark
+    public void gson() {
+
+        final InventoryItem item = InventoryItem.builder().id(UUID.randomUUID()).description("item4test").available(0).build();
+        final InventoryItem identicalItem = InventoryItem.builder().id(item.getId()).description(item.getDescription()).available(item.getAvailable()).build();
+        final Snapshot<InventoryItem> snapshot = new JdbiGsonSnapshot<>(InventoryItem.class, item, 0L, gson);
+        assertThat(snapshot.getAggregateInstance(), is(item));
+        final InventoryItem itemFromSnapshot = snapshot.getAggregateInstance();
+        itemFromSnapshot.setDescription("notAnymore");
+        assertThat(snapshot.getAggregateInstance(), is(identicalItem));
+
+    }
+
+}
