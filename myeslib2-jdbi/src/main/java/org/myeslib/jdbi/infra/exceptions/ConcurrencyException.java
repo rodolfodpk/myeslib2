570f8f9f33b296ab63e237aa2f3af26c74b573c4
Rodolfo de Paula <rpaula.example@git.vmcommerce.intra>
2015-02-09 19:25:14 -0200

    renamed myeslib2-jdbi to myeslib2-stack1

--- myeslib2-jdbi/src/main/java/org/myeslib/jdbi/infra/exceptions/ConcurrencyException.java
+++
@@ -1,7 +0,0 @@
-package org.myeslib.jdbi.infra.exceptions;
-
-public class ConcurrencyException extends InfraRuntimeException {
-    public ConcurrencyException(String s) {
-        super(s);
-    }
-}
--- myeslib2-jdbi/src/main/java/org/myeslib/jdbi/infra/exceptions/InfraRuntimeException.java
+++
@@ -1,7 +0,0 @@
-package org.myeslib.jdbi.infra.exceptions;
-
-public class InfraRuntimeException extends RuntimeException {
-    public InfraRuntimeException(String s) {
-        super(s);
-    }
-}
