501a8bd2e8c93055597773798edf767c00b7f1d0
Rodolfo de Paula <rpaula.example@git.vmcommerce.intra>
2015-03-03 16:47:06 -0300

    moved DatabaseHelper class up to java folder

--- myeslib2-stack1/src/main/java/org/myeslib/stack1/infra/helpers/DatabaseHelper.java
+++ myeslib2-stack1/src/main/java/org/myeslib/stack1/infra/helpers/DatabaseHelper.java
@@ -4,9 +4,10 @@ import com.google.common.base.CharMatcher;
 import com.google.common.base.Charsets;
 import com.google.common.base.Splitter;
 import com.google.common.io.Resources;
-import lombok.extern.slf4j.Slf4j;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URL;
@@ -14,11 +15,12 @@ import java.net.URL;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 
-@Slf4j
 public class DatabaseHelper {
 
     private final DBI dbi;
     private final String ddlScriptFile;
+
+    static final Logger logger = LoggerFactory.getLogger(DatabaseHelper.class);
 
     public DatabaseHelper(DBI dbi, String ddlScriptFile) {
         checkNotNull(dbi);
@@ -31,7 +33,7 @@ public class DatabaseHelper {
         try {
             Handle h = dbi.open();
             for (String statement : statements()) {
-                log.debug("executing {}", statement);
+                logger.debug("executing {}", statement);
                 h.execute(statement);
             }
             h.close();
