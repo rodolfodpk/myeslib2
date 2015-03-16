c76bed6bb2bea6f51ed7a3c971dbcff72fe74bcd
Rodolfo de Paula <rodolfo_dpk@yahoo.com>
2015-03-16 18:38:46 -0300

    moved Snapshot to infra package

---
+++ myeslib2-core/src/main/java/org/myeslib/infra/Snapshot.java
@@ -0,0 +1,14 @@
+package org.myeslib.infra;
+
+import net.jcip.annotations.Immutable;
+import org.myeslib.core.AggregateRoot;
+
+import java.io.Serializable;
+
+@Immutable
+public interface Snapshot<A extends AggregateRoot> extends Serializable {
+
+    A getAggregateInstance();
+
+    Long getVersion();
+}
