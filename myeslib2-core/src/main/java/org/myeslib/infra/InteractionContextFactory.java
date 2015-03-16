cf08350dfcb350e76d139278238625585788606a
Rodolfo de Paula <rodolfo_dpk@yahoo.com>
2015-03-16 01:01:45 -0300

    deleted InteractionContext factories

--- myeslib2-core/src/main/java/org/myeslib/infra/InteractionContextFactory.java
+++
@@ -1,8 +0,0 @@
-package org.myeslib.infra;
-
-import org.myeslib.core.AggregateRoot;
-
-import java.util.function.Function;
-
-public interface InteractionContextFactory<A  extends AggregateRoot> extends Function<A, InteractionContext> {
-}
--- myeslib2-core/src/main/java/org/myeslib/infra/SagaInteractionContextFactory.java
+++
@@ -1,8 +0,0 @@
-package org.myeslib.infra;
-
-import org.myeslib.core.AggregateRoot;
-
-import java.util.function.Function;
-
-public interface SagaInteractionContextFactory<A  extends AggregateRoot> extends Function<A, SagaInteractionContext> {
-}
