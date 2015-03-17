6a12f141d3b8ab829071c4addfbeb12d52f9ebb8
Rodolfo de Paula <rodolfo_dpk@yahoo.com>
2015-03-17 17:17:06 -0300

    replaced AggregateRoot with EventSourced

--- myeslib2-stack1/src/main/java/org/myeslib/stack1/infra/Stack1ApplyEventsFunction.java
+++ myeslib2-stack1/src/main/java/org/myeslib/stack1/infra/Stack1ApplyEventsFunction.java
@@ -1,13 +1,13 @@
 package org.myeslib.stack1.infra;
 
-import org.myeslib.core.AggregateRoot;
+import org.myeslib.core.EventSourced;
 import org.myeslib.data.Event;
 import org.myeslib.infra.ApplyEventsFunction;
 import org.myeslib.stack1.infra.helpers.MultiMethod;
 
 import java.util.List;
 
-public class Stack1ApplyEventsFunction<A extends AggregateRoot> implements ApplyEventsFunction<A> {
+public class Stack1ApplyEventsFunction<A extends EventSourced> implements ApplyEventsFunction<A> {
 
     @Override
     public A apply(A a, List<Event> events) {
@@ -15,7 +15,7 @@ public class Stack1ApplyEventsFunction<A extends AggregateRoot> implements Apply
         return a;
     }
 
-    private void _applyEventsOn(AggregateRoot instance, List<? extends Event> events) {
+    private void _applyEventsOn(EventSourced instance, List<? extends Event> events) {
         MultiMethod mm = MultiMethod.getMultiMethod(instance.getClass(), "on");
         for (Event event : events) {
             try {
