c187e95d0e9503f2d6b92178c1479ae9a6c640ba
Rodolfo de Paula <rodolfo_dpk@yahoo.com>
2015-03-22 02:05:57 -0300

    modularity refctoring

---
+++ myeslib2-core/src/main/java/org/myeslib/infra/dao/config/CmdSerialization.java
@@ -0,0 +1,18 @@
+package org.myeslib.infra.dao.config;
+
+import org.myeslib.data.Command;
+
+import java.util.function.Function;
+
+public class CmdSerialization {
+
+    public final Function<Command, String> toStringFunction;
+    public final Function<String, Command> fromStringFunction;
+
+    public CmdSerialization(Function<Command, String> toStringFunction,
+                            Function<String, Command> fromStringFunction) {
+        this.toStringFunction = toStringFunction;
+        this.fromStringFunction = fromStringFunction;
+    }
+
+}
